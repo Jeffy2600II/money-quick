72202c59eae2e766eefe9f2799a1b8081fc282a2
Jeffy2600II <nontakorn2600@gmail.com>
2025-12-26 21:18:29 +0700

    update pin.ts

--- lib/pin.ts
+++ lib/pin.ts
@@ -5,7 +5,6 @@ const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL!,
   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 });
-
 const PIN_KEY = "pin-hash";
 
 export async function setPin(pin: string) {
