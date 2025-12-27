fe0b61173e5ae77fea20410e100fe0d144453c31
Jeffy2600II <nontakorn2600@gmail.com>
2025-12-27 20:17:38 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,28 +2,16 @@
 import '../styles/globals.css';
 import PopupProvider from '../components/PopupProvider';
 import LoaderProvider from '../components/LoaderProvider';
-import ServiceWorkerRegister from '../components/ServiceWorkerRegister';
-import { SWRConfig } from 'swr';
-import { fetchWithTimeout } from '../lib/fetcher';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="th">
-      <head>
-        {/* If you have a dedicated API hostname, consider adding preconnect here:
-            <link rel="preconnect" href="https://api.example.com" />
-            (left out because origin may be same)
-        */}
-      </head>
       <body>
-        <SWRConfig value={{ fetcher: (resource: string, init?: any) => fetchWithTimeout(resource, init) }}>
-          <PopupProvider>
-            <LoaderProvider>
-              {children}
-              <ServiceWorkerRegister />
-            </LoaderProvider>
-          </PopupProvider>
-        </SWRConfig>
+        <PopupProvider>
+          <LoaderProvider>
+            {children}
+          </LoaderProvider>
+        </PopupProvider>
       </body>
     </html>
   );
